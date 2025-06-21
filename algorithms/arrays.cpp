83765a5be3c3015132d7bf012783b06a567a629f
Alokananda Y <plantsfortrees@gmail.com>
2025-06-21 06:55:15 +0530

    Working with variables

--- algorithms/arrays.cpp
+++ algorithms/arrays.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
     int my_array[5] = {1, 2, 3, 4, 5};
-    cout << my_array[2] << endl; 
-    // change the last element to something else
-    int array_length = sizeof(my_array);
-    cout << array_length << endl;
+
+    // Print the 3rd element (index 2)
+    cout << my_array[2] << endl;
+
+    // Change the last element
+    my_array[4] = 10;
+
+    // Calculate number of elements
+    int array_length = sizeof(my_array) / sizeof(my_array[0]);
+    cout << "Array length: " << array_length << endl;
+
+    // Print the updated array
+    for (int i = 0; i < array_length; i++)
+    {
+        cout << my_array[i] << " ";
+    }
+    cout << endl;
 }
---
+++ learn_c/21-06-25/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void explainVariables()
+{
+    // Integer variable
+    int age = 45;
+    printf("Integer: age = %d\n", age);
+}
+
+int main(int argc, const char *argv[])
+{
+    explainVariables();
+    return 0;
+}
