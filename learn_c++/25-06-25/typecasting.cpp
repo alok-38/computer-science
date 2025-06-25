4a2aeb97781361be988028c983f27e6552dfaff0
Alokananda Y <plantsfortrees@gmail.com>
2025-06-25 20:49:53 +0530

    type casting

---
+++ learn_c++/25-06-25/typecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    float b = 2.3;
+    float result = a + b;
+    cout << result << endl;
+    int a_1 = 100000;
+    long b_1 = (long)a_1 * a_1;
+    cout << "Long: " << b << endl;
+}
