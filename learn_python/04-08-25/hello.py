5e0af7ea7fdfcd15d55fc0bbc398100a6abf0b17
plantsfortrees@gmail.com <Alokananda Y>
2025-08-03 21:29:41 +0000

    Coding for AI

---
+++ learn_python/04-08-25/hello.py
@@ -0,0 +1,5 @@
+name = input("May I know your name please: ").strip()
+print(f"Hello, {name}")
+# Say hello to someone you love
+someone = input("Say 'Hello' to someone you love: ").strip()
+print(f"I love my {someone}")
