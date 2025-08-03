20fa2146707cf79080117354a4eb820863e9d070
plantsfortrees@gmail.com <Alokananda Y>
2025-08-03 08:14:23 +0000

    Working with vectors

--- learn_python/03-08-25/vectors.py
+++ learn_python/03-08-25/vectors.py
@@ -36,3 +36,31 @@ print(f"In this case weighted average equals regular average: {weighted_average}
 # Lets add some weights
 weights = [0.1, 0.2, 0.3, 0.4]
 
+weighted_sum = 0
+for value, weight in zip(v, weights):
+    weighted_sum += value * weight
+
+weighted_average = weighted_sum / sum(weights)
+print(f"Weighted average: {weighted_average}")
+
+# Square each element
+squares = []
+
+for each_value in v:
+    squares.append(each_value * each_value)
+
+print(f"After squaring: {squares}")
+
+# Convert to another type (e.g., tuple, NumPy array)
+my_tuple = tuple(v)
+print(f"After converting to a tuple: {my_tuple}")
+
+import numpy as np
+
+my_list = [1, 2, 3, 4]
+my_array = np.array(my_list)
+
+import numpy as np
+my_np_array = np.array(v)
+print(f"After converting to np array: {my_np_array}")
+
